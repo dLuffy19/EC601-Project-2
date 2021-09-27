37e7c28fc452133d0dec6b95961287b47e4895c3
dLuffy19 <52364956+dLuffy19@users.noreply.github.com>
2021-09-27 00:05:53 -0400

    Update request.py

--- request.py
+++ request.py
@@ -8,10 +8,7 @@ consumer_secret = os.environ.get("CONSUMER_SECRET")
 access_token_key = os.environ.get("ACCESS_TOKEN_KEY")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
-api = TwitterAPI(consumer_key=consumer_key, 
-	             consumer_secret=consumer_secret, 
-				 access_token_key=access_token_key, 
-	             access_token_secret=access_token_secret)
+api = TwitterAPI(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token_key=access_token_key, access_token_secret=access_token_secret)
 
 r = api.request('search/tweets', {'q':'basketball'})
 print (r.status_code)
